1cfb90666d842af466b18fbb8a1b38017738751f
renataubueno <renataubueno@gmail.com>
2019-05-25 09:45:45 -0300

    commented the app.js and simulacao.js files; created a back up file of simulacaoService.js because I'll start its generalization from scratch

--- routes/simulacao.js
+++ routes/simulacao.js
@@ -1,32 +1,40 @@
 var express = require('express');
-var random = require('random');
-var seedrandom = require('seedrandom');
+/* requisição do serviço que realmente calcula a simulação */
 var serviceSimulacao = require('../services/simulacaoService');
 var router = express.Router();
 
+/* Confirmar se este GET é necessário */
 router.get('/', function(req, res, next) {
   res.status(200);
   res.send(serviceSimulacao.simulacaoGET());
 });
 
 router.post('/', function(req, res, next) {
+  /* recebimento do body do front end, com as informações necessárias para calcular a simulação */
   let body = req.body;
 
+  /* primeiro é feita a validação dos dados recebidos */
+  /* apesar de já ser feita uma validação no front, devemos conferir se os mesmos dados são recebidos no back end */
   if(validar(body)){
     console.log('BODY: ', body);
     let retorno = serviceSimulacao.simulacaoPOST(body);
     console.log('RETORNO: ', retorno);
     res.send(retorno);
   } else {
-    console.log('DEU ERRO');
+    console.log('Erro na Validação do Back End');
     res.status(400);
     res.jsonp('Erro na validação do back end');
   }
 });
 
+/* Função para fazer a validação de alguns dados que chegam no back end */
 function validar(dado){
-  console.log('VALIDANDO DADO NO BACK END: ', dado);
-  return true;
+  /* Verifica se os dados recebidos não estão nulos ou vazios */
+  if(dado.objSimulacao.length === 0 || dado.seeder === null || dado.condParada === null || dado.tipoParada === null){
+    return false;
+  } else {
+    return true;
+  }
 }
 
 module.exports = router;
--- services/simulacaoService.js
+++ services/simulacaoService.js
@@ -2,6 +2,8 @@ var random = require('random');
 var seedrandom = require('seedrandom');
 
 let escalonador = [];
+let numChegadasMax = 0;
+let tempoMax = 0;
 
 exports.simulacaoGET = function () {
   return {
@@ -21,8 +23,14 @@ exports.simulacaoPOST = function (dados) {
   let entrada = dados.objSimulacao.filter(item => item.tipo === 'ENTRADA');
   let saida = dados.objSimulacao.filter(item => item.tipo === 'SAIDA');
 
-  let numChegadasMax = dados.condParadaNumChegadas;
   let seed = dados.seeder;
+
+  if(dados.tipoParada === 'CHEGADAS'){
+    numChegadasMax = dados.condParada;
+  } else {
+    tempoMax = dados.condParada;
+  }
+
 
   console.log('ANTES DE ENTRAR NA SIMULACAO');
   let resultado = simulacao(fila, entrada, numChegadasMax, seed);
@@ -52,167 +60,23 @@ exports.simulacaoPOST = function (dados) {
   };
 }
 
+function simulacao(fila, entrada, numChegadasMax, seed){
+  console.log('NA SIMULACAO');
+
+  return {
+    numChegadas: 10,
+    numAtendimentos: 12,
+    tempoOcupada: 13,
+    tempoTotal: 11,
+    probEstadosFila: [1, 2, 3, 4]
+  };
+}
+
 function geraAleatorio(){
   let numAleatorio = random.float(min = 0, max = 1);
   random.next();
 
   return numAleatorio;
-}
-
-function simulacao(fila, entrada, numChegadasMax, seed){
-  console.log('NA SIMULACAO');
-  //aleatorio
-  random.use(seedrandom(seed));
-  let numAleatorio = geraAleatorio();
-  let agendaChegada = 0;
-  let agendaSaida = 0;
-  let tempo = 0;
-  //tenho que pegar todas as infos que preciso dos objetos e salvar em objetos locais
-  let minChegada = fila[0].minChegada;
-  let maxChegada = fila[0].maxChegada;
-  let minServico = fila[0].minServico;
-  let maxServico = fila[0].maxServico;
-  let capacidade = fila[0].capacidade;
-  let servidores = fila[0].servidores;
-  let chegada = entrada[0].chegada;
-  //criar variável de controle pra quantidade de usuários na fila e que já tenham sido atendidos (agendada a saida)
-  let condicaoFila = 0;
-  let probEstadosFila = Array(capacidade+1);
-  let momentoAnterior = 0;
-  let momentoAtual = 0;
-  //criar o escalonador
-  escalonador = [];
-  escalonador.push({
-    momento: chegada,
-    tipo: 'CHEGADA'});
-  //criar as variaveis de retorno: numChegadas, numAtendimentos e tempoOcupada
-  let numChegadas = 0;
-  let numAtendimentos = 0;
-  //algoritmo
-
-  while(escalonador.length !== 0 && numChegadas < numChegadasMax){
-
-    console.log('ESCALONADOR', escalonador);
-    //console.log('NUMCHEGADAS', numChegadas);
-    //console.log('NUMATENDIMENTOS', numAtendimentos);
-
-    let next = escalonador[0];
-    escalonador.splice(0, 1);
-    if(next.tipo === 'CHEGADA'){
-      //verificar quanto tempo a fila ficou com capacidade = 0, 1, 2...
-      if(probEstadosFila[condicaoFila] === undefined){
-        momentoAtual = next.momento - momentoAnterior;
-        momentoAnterior = next.momento;
-        probEstadosFila[condicaoFila] = momentoAtual;
-      } else {
-        momentoAtual = next.momento - momentoAnterior;
-        momentoAnterior = next.momento;
-        probEstadosFila[condicaoFila] = probEstadosFila[condicaoFila] + momentoAtual;
-      }
-
-
-      numChegadas++;
-      if(numChegadas === 5){
-        break;
-      }
-      if(condicaoFila < capacidade){
-        condicaoFila++;
-        if(condicaoFila <= servidores){
-          agendaSaida = next.momento + uniforme(minServico, maxServico);
-
-          if(escalonador.length === 0){
-            escalonador.push({
-              momento: agendaSaida,
-              tipo: 'SAIDA'
-            });
-          } else {
-            for(let i = 0; i < escalonador.length; i++){
-              if(escalonador.length === i+1 && escalonador[i].momento < agendaSaida){
-                escalonador.push({
-                  momento: agendaSaida,
-                  tipo: 'SAIDA'
-                });
-                break;
-              } else if(escalonador[i].momento > agendaSaida){
-                escalonador.splice(i, 0, {momento: agendaSaida, tipo: 'SAIDA'});
-                break;
-              }
-            }
-          }
-        };
-      };
-
-      agendaChegada =  next.momento + uniforme(minChegada, maxChegada);
-
-      if(escalonador.length === 0){
-        escalonador.push({
-          momento: agendaChegada,
-          tipo: 'CHEGADA'});
-      } else {
-        for(let i = 0; i < escalonador.length; i++){
-          if(escalonador.length === i+1 && escalonador[i].momento < agendaChegada){
-            escalonador.push({
-              momento: agendaChegada,
-              tipo: 'CHEGADA'});
-            break;
-          } else if(escalonador[i].momento > agendaChegada){
-            escalonador.splice(i, 0, {momento: agendaChegada,tipo: 'CHEGADA'});
-            break;
-          }
-        }
-      }
-    } else {
-      if(probEstadosFila[condicaoFila] === undefined){
-        momentoAtual = next.momento - momentoAnterior;
-        momentoAnterior = next.momento;
-        probEstadosFila[condicaoFila] = momentoAtual;
-      } else {
-        momentoAtual = next.momento - momentoAnterior;
-        momentoAnterior = next.momento;
-        probEstadosFila[condicaoFila] = probEstadosFila[condicaoFila] + momentoAtual;
-      }
-
-      numAtendimentos++;
-      condicaoFila--;
-      if(condicaoFila >= 1){
-        agendaSaida = next.momento + uniforme(minServico, maxServico);
-
-        if(escalonador.length === 0){
-          escalonador.push({
-            momento: agendaSaida,
-            tipo: 'SAIDA'
-          });
-        } else {
-          for(let i = 0; i < escalonador.length; i++){
-            if(escalonador.length === i+1 && escalonador[i].momento < agendaSaida){
-              escalonador.push({
-                momento: agendaSaida,
-                tipo: 'SAIDA'
-              });
-              break;
-            } else if(escalonador[i].momento > agendaSaida){
-              escalonador.splice(i, 0, {momento: agendaSaida, tipo: 'SAIDA'});
-              break;
-            }
-          }
-        }
-      }
-    }
-  }
-
-  let tempoTotal = momentoAnterior;
-  let tempoOcupada = momentoAnterior - probEstadosFila[0];
-  //console.log('TEMPO TOTAL: ', tempoTotal);
-  //console.log('TEMPO OCUPADA: ', tempoOcupada);
-  console.log('PROBABILIDADE DE ESTADOS DA FILA - FINAL: ', probEstadosFila);
-
-  return {
-    numChegadas: numChegadas,
-    numAtendimentos: numAtendimentos,
-    tempoOcupada: tempoOcupada,
-    tempoTotal: tempoTotal,
-    probEstadosFila: probEstadosFila
-  };
 }
 
 function uniforme(min, max){
